ac4326bb1bad8352c00cff2a0e4d74d6a7058103
Kasun madhushan wickramasinghe <119012876+KAXUN01@users.noreply.github.com>
2025-01-02 13:45:40 +0000

    update contact component

--- src/components/contact-us.tsx
+++ src/components/contact-us.tsx
@@ -12,7 +12,7 @@ export const ContactUs = () => {
       <div className="p-4 md:p-8 md:px-16">
         <div className="grid md:grid-cols-5 gap-1 md:pt-1 mt-auto">
           {/* Community Links Section */}
-          <div className="flex flex-col items-start space-y-6 py-6 md:space-y-0 px-10 md:gap-8">
+          <div className="flex flex-col items-start space-y-6 py-6 md:space-y-0 px-10 md:px-0 md:gap-4">
             <h3 className="text-xl font-bold mb-2">Community</h3>
             <a
               href="https://medium.com/@kaxun"
@@ -41,19 +41,10 @@ export const ContactUs = () => {
               <FaFigma />
               <span>Figma</span>
             </a>
-            <a
-              href="https://stackoverflow.com/users/21034982/kaxun"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center space-x-2 text-gray-300 hover:text-white transition transform hover:scale-105"
-            >
-              <FaStackOverflow />
-              <span>Stack Overflow</span>
-            </a>
           </div>
 
           {/* Social Media Links Section */}
-          <div className="flex flex-col items-start space-y-6 md:space-y-0 px-10 py-6 md:gap-8">
+          <div className="flex flex-col items-start space-y-6 md:space-y-0 px-10 py-6 md:gap-4 md:px-0">
             <h3 className="text-xl font-bold mb-2">Social Media</h3>
             <a
               href="https://www.linkedin.com/in/kaxun01/"
@@ -85,7 +76,7 @@ export const ContactUs = () => {
           </div>
 
           {/* Email Section */}
-          <div className="flex flex-col items-start px-10 md:px-0 py-6 md:w-full">
+          <div className="flex flex-col items-start px-10 md:px-0 md:px-0 py-6 md:w-full">
             <div className="pb-4">
               <Transition>
                 <span className="text-white/90">Get in touch</span>
